c97143b9958981a6657d276e3a4ac81442bd1787
mytreo <mytreogm@gmail.com>
2015-10-16 22:59:26 +0300

    Initialize rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aptproject::Application.config.secret_key_base = '58cb162dd54892d3e1c529204d73c477e20eca8d93f0bc5ac1dc4820ae347651d00619b127732ce734c6d0a53b07be94ee069f46efd86e7c687da48ded7d4f2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Aptproject::Application.config.session_store :cookie_store, key: '_Aptproject_session'
